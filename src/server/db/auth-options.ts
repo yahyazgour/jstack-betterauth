f5d91e427cc4d963c688c579faf92678c909fedd
yahyazgour <zgour.yahya@gmail.com>
2025-03-25 21:17:37 +0000

    Update trustedOrigins in auth options to allow all origins

--- src/server/db/auth-options.ts
+++ src/server/db/auth-options.ts
@@ -17,7 +17,7 @@ export const betterAuthOptions = (
     appName: "discord-opgg",
     baseURL: betterAuthUrl,
     secret: betterAuthSecret,
-    trustedOrigins: [clientHost],
+    trustedOrigins: ["*"],
     session: {
       expiresIn: 60 * 60 * 24 * 7, // 7 days
       updateAge: 60 * 60 * 24, // 1 day (every 1 day the session expiration is updated)
